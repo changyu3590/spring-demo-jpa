376daa99eb64e5c2f05a02888dca931a00dff6de
CHANG Yu <yu.a.chang@capgeminigemini.com>
2019-07-11 12:44:45 +0800

    JPA搭建

--- src/main/java/com/example/demo/repository/UserDao.java
+++ src/main/java/com/example/demo/repository/UserDao.java
@@ -4,14 +4,11 @@ package com.example.demo.repository;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 
 /**
  * @Author: ChangYu
  * @Version 1.0
  */
 public interface UserDao extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-    @Query(value = "SELECT * FROM t_user where username=?1", nativeQuery = true)
     User findByUsername(String username);
-
 }
